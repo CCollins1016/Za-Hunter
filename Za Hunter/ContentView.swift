d7c3ed991972ae04dc5613e5184f182843f0f97b
CCollins1016 <ccollins@bsd220.org>
2026-02-17 10:42:10 -0600

    setup initial map

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
+    @State private var mapRegion = MKCoordinateRegion()
     var body: some View {
-        VStack {
+        NavigationView {
+            Map(position: $startPosition) {
+            UserAnnotation()
+            }
+            .navigationBarTitle("'Za Hunter", displayMode: .inline)
+            .toolbarBackground(.hidden, for: .navigationBar)
         }
     }
 }
